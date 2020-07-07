1a89d1a0d44d734d92e7af075a78d223d5fb62eb
thomaspascoe <thomas.pascoe@bglgroup.co.uk>
2020-07-07 16:58:10 +0100

    update single responsibility

--- SingleResponsibility.js
+++ SingleResponsibility.js
@@ -1,16 +1,42 @@
-class Person {
-    constructor(name, age, alive) {
+var Employee = /** @class */ (function () {
+    function Employee(name, payPerHour, pensionContribution, tax) {
         this.name = name;
-        this.age = age;
-        this.alive = alive;
+        this.payPerHour = payPerHour;
+        this.pensionContribution = pensionContribution;
+        this.tax = tax;
     }
-    personStatus() {
-        console.log("Name : " + this.name);
-        console.log("Age : " + this.age);
-        console.log("Alive : " + this.alive);
-    }
-}
-let dave = new Person("Dave", 40, true);
-console.log(dave);
-dave.personStatus();
-//# sourceMappingURL=SingleResponsibility.js.map
+    //Example - Bad
+    Employee.prototype.payEmployee = function (hoursWorked) {
+        var grossPay = this.payPerHour * hoursWorked;
+        var pensionPayment = grossPay / this.pensionContribution;
+        var taxPayment = grossPay / this.tax;
+        var netPay = grossPay - pensionPayment - taxPayment;
+        console.log(this.name + "'s net payment was £" + netPay);
+    };
+    //Example - Good
+    Employee.prototype.payEmployeeSRP = function (hoursWorked) {
+        var grossPay = this.calculateGrossPayment(hoursWorked);
+        var pensionPayment = this.calculatePensionPayment(grossPay);
+        var taxPayment = this.calculateTaxPayment(grossPay);
+        var netPay = this.calculateNetPayment(grossPay, pensionPayment, taxPayment);
+        console.log(this.name + "'s net payment was £" + netPay);
+    };
+    Employee.prototype.calculateGrossPayment = function (hoursWorked) {
+        return this.payPerHour * hoursWorked;
+    };
+    Employee.prototype.calculatePensionPayment = function (grossPay) {
+        return grossPay / this.pensionContribution;
+    };
+    Employee.prototype.calculateTaxPayment = function (grossPay) {
+        return grossPay / this.tax;
+    };
+    Employee.prototype.calculateNetPayment = function (grossPay, pensionPayment, taxPayment) {
+        return grossPay - pensionPayment - taxPayment;
+    };
+    return Employee;
+}());
+var dave = new Employee("Dave", 10, 10, 10);
+//Example - Bad
+dave.payEmployee(37);
+//Example - Good
+dave.payEmployeeSRP(37);
--- SingleResponsibility.ts
+++ SingleResponsibility.ts
@@ -1,23 +1,53 @@
-class Person {
+class Employee {
     name: string;
-    age: number;
-    alive: boolean;
+    payPerHour: number;
+    pensionContribution: number;
+    tax: number;
 
-    constructor(name: string, age: number, alive: boolean) {
+    constructor(name: string, payPerHour: number, pensionContribution: number, tax: number) {
         this.name = name;
-        this.age = age;
-        this.alive = alive;
+        this.payPerHour = payPerHour;
+        this.pensionContribution = pensionContribution;
+        this.tax = tax;
     }
 
-    personStatus(): void {
-        console.log("Name : " + this.name);
-        console.log("Age : " + this.age);
-        console.log("Alive : " + this.alive);
+    //Example - Bad
+    payEmployee(hoursWorked: number): void {
+        let grossPay: number = this.payPerHour * hoursWorked;
+        let pensionPayment: number = grossPay / this.pensionContribution;
+        let taxPayment: number = grossPay / this.tax;
+        let netPay: number = grossPay - pensionPayment - taxPayment;
+        console.log(this.name + "'s net payment was £" + netPay);
+    }
+
+    //Example - Good
+    payEmployeeSRP(hoursWorked: number): void {
+        let grossPay: number = this.calculateGrossPayment(hoursWorked);
+        let pensionPayment: number = this.calculatePensionPayment(grossPay);
+        let taxPayment: number = this.calculateTaxPayment(grossPay);
+        let netPay: number = this.calculateNetPayment(grossPay, pensionPayment, taxPayment);
+        console.log(this.name + "'s net payment was £" + netPay);
+    }
+
+    calculateGrossPayment(hoursWorked: number): number {
+        return this.payPerHour * hoursWorked;
+    }
+
+    calculatePensionPayment(grossPay): number {
+        return grossPay / this.pensionContribution;
+    }
+
+    calculateTaxPayment(grossPay): number {
+        return grossPay / this.tax;
+    }
+
+    calculateNetPayment(grossPay: number, pensionPayment: number, taxPayment: number): number {
+        return grossPay - pensionPayment - taxPayment;
     }
 }
 
-let dave = new Person("Dave", 40, true);
-
-console.log(dave);
-
-dave.personStatus();
+let dave = new Employee("Dave", 10, 10, 10);
+//Example - Bad
+dave.payEmployee(37);
+//Example - Good
+dave.payEmployeeSRP(37);
